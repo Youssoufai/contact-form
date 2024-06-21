9f4eb7e1e6d4f8c79fb00f5ed3525ef6633d8ff8
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-06-21 23:36:59 +0100

    Initialize repo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    @vite('resources/css/app.css', 'resources/js/app.js')
+</head>
+
+<body>
+    <form action="">
+        <h1 class="text-3xl">Contact Us</h1>
+
+    </form>
+</body>
+
+</html>
